298fd97852024c6ab8e3e2778614137288424715
Pedro Jorquera <pjorquera@okode.com>
2015-05-23 19:13:57 +0200

    Added common dependency for iOS App

--- common/src/main/java/com/okode/demo/common/Common.java
+++ common/src/main/java/com/okode/demo/common/Common.java
@@ -2,15 +2,7 @@ package com.okode.demo.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGetHC4;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Map;
 
 public class Common {
@@ -33,25 +25,4 @@ public class Common {
         return value;
     }
 
-    public static String getValueFromURL(String url, String key) {
-        String json = "";
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGetHC4 httpGet = new HttpGetHC4("http://targethost/homepage");
-        try {
-            CloseableHttpResponse response = httpclient.execute(httpGet);
-            InputStream in = response.getEntity().getContent();
-            InputStreamReader is = new InputStreamReader(in);
-            StringBuilder sb=new StringBuilder();
-            BufferedReader br = new BufferedReader(is);
-            String read = br.readLine();
-            while(read != null) {
-                sb.append(read);
-                read =br.readLine();
-            }
-            json = sb.toString();
-        } catch (IOException e) {
-            System.out.println("Could not get content from URL '" + url + "': " + e.getMessage());
-        }
-        return getValueFromJson(json, key);
-    }
 }
