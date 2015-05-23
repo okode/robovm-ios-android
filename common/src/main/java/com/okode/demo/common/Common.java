4ef5f79df357463feaa01c86edaf195f19c90cb3
Pedro Jorquera <pjorquera@okode.com>
2015-05-23 13:31:26 +0200

    Added HTTP Client (+ HC Fluent) dependency

--- common/src/main/java/com/okode/demo/common/Common.java
+++ common/src/main/java/com/okode/demo/common/Common.java
@@ -1,9 +1,39 @@
 package com.okode.demo.common;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class Common {
 
     public static void showMessage(String message) {
         System.out.println(message);
     }
 
+    public static String getValueFromJson(String json, String key) {
+        String value = "null";
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map values = mapper.readValue(json, Map.class);
+            if (values.containsKey(key)) {
+                value = values.get(key).toString();
+            }
+        } catch (IOException e) {
+            System.out.println("Could not parse json: " + json);
+        }
+        return value;
+    }
+
+    public static String getValueFromURL(String url, String key) {
+        String json = "";
+        try {
+            json = Request.Get(url).execute().returnContent().asString();
+        } catch (IOException e) {
+            System.out.println("Could not retrieve contents from URL: " + url);
+        }
+        return getValueFromJson(json, key);
+    }
 }
